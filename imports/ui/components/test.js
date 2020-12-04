b7ef797f06582976f3b8c5579fc3c84cbd21260e
Minh Nguyen <minhna@gmail.com>
2020-12-04 10:08:27 +0700

    works with react-bootstrap-icons

--- imports/ui/components/test.js
+++ imports/ui/components/test.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import { Button } from "react-bootstrap";
+import { Alarm } from "react-bootstrap-icons";
 
 function TestComponent() {
   return (
     <div>
+      <Alarm />
       <Button>Click me</Button>
     </div>
   );
