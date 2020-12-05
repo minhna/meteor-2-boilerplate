669979aba990e01103671d32c04fb9812f029ac3
Minh Nguyen <minhna@gmail.com>
2020-12-05 17:02:18 +0700

    add 404 page

--- imports/ui/routes/index.js
+++ imports/ui/routes/index.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 const HomePage = lazy(() => import("/imports/ui/pages/home.js"));
 const TestPage = lazy(() => import("/imports/ui/pages/test.js"));
+const NotFoundPage = lazy(() => import("/imports/ui/pages/not-found.js"));
 
 export default MainRoutes = () => (
   <Suspense fallback={<div>loading...</div>}>
     <Switch>
       <Route path="/test" component={TestPage} />
-      <Route component={HomePage} />
+      <Route path="/" exact component={HomePage} />
+      <Route component={NotFoundPage} />
     </Switch>
   </Suspense>
 );
